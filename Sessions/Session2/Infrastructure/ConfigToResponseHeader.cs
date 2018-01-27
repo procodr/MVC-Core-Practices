0d1a2f15739020119174cc721d7f4a56331adc84
ProCodr <malireza@ymail.com>
2018-01-27 12:06:28 +0330

    Add Projec Files

---
+++ Sessions/Session2/Infrastructure/ConfigToResponseHeader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Session2.Infrastructure
+{
+    public class ConfigToResponseHeader
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public ConfigToResponseHeader(RequestDelegate next, IConfigurationRoot configurationRoot)
+        {
+            _next = next;
+            _configurationRoot = configurationRoot;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("X-Powered-By", _configurationRoot.GetSection("ResponseHeaders")["X-Powered-By"]);
+            httpContext.Response.Headers.Add("H1", _configurationRoot.GetSection("ResponseHeaders")["H1"]);
+            httpContext.Response.Headers.Add("H2", _configurationRoot.GetSection("ResponseHeaders")["H2"]);
+
+            await httpContext.Response.WriteAsync("Hello from ConfigToResponseHeader\nResponse Editing Middleware:)");
+        }
+    }
+}
---
+++ Sessions/Session2/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Session2.Infrastructure;
+
+namespace Session2
+{
+    public class Startup
+    {
+        IConfigurationRoot _configurationRoot;
+        public Startup(IHostingEnvironment env)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                .Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IConfigurationRoot), _configurationRoot);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMiddleware<ConfigToResponseHeader>();
+        }
+    }
+}
